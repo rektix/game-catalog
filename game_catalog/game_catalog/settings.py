db4ec4bcf1d42695330093fbb435c9596f46efb5
MihajloKrsmanovic <mihajlo@dataandmore.com>
2021-01-04 15:34:14 +0100

    Modify settings and add base templates

--- game_catalog/game_catalog/settings.py
+++ game_catalog/game_catalog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'game_catalog_app.apps.GameCatalogAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'game_catalog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', os.path.join(BASE_DIR, 'game_catalog_app/../templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ game_catalog/game_catalog_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GameCatalogAppConfig(AppConfig):
+    name = 'game_catalog_app'
